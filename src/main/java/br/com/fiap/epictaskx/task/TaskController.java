03ec8eb0c583e810097903af23aa157034e97fb0
joaocarloslima <joaocarloslima@me.com>
2025-08-11 20:56:10 -0300

    add task creation form and add css style

--- src/main/java/br/com/fiap/epictaskx/task/TaskController.java
+++ src/main/java/br/com/fiap/epictaskx/task/TaskController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -20,9 +22,20 @@ public class TaskController {
 
     @GetMapping
     public String index(Model model){
-
         model.addAttribute("tasks", taskService.getAllTasks());
         return "index";
+    }
+
+    @GetMapping("/form")
+    public String form(){
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String create(Task task, RedirectAttributes redirect){ //TODO DTO
+        taskService.save(task);
+        redirect.addFlashAttribute("message", "Tarefa cadastrada com sucesso !");
+        return "redirect:/task";
     }
 
 }
