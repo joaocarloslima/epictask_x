3404d0a7a0c4c51ddd42ffd88345f295a62c40b0
joaocarloslima <joaocarloslima@me.com>
2025-08-25 20:48:00 -0300

    add authentication support with OAuth2 and create login/logout pages

--- src/main/java/br/com/fiap/epictaskx/task/TaskController.java
+++ src/main/java/br/com/fiap/epictaskx/task/TaskController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,8 +26,13 @@ public class TaskController {
     private final MessageSource messageSource;
 
     @GetMapping
-    public String index(Model model){
+    public String index(Model model, @AuthenticationPrincipal OAuth2User user){
+        var avatar = user.getAttributes().get("picture") != null ?
+                        user.getAttributes().get("picture") :
+                        user.getAttributes().get("avatar_url");
         model.addAttribute("tasks", taskService.getAllTasks());
+        model.addAttribute("user", user);
+        model.addAttribute("avatar", avatar);
         return "index";
     }
 
